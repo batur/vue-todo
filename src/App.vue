edfdfbbaaa41c84408cc30d21b0f63120d4e1283
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-10-05 22:25:36 +0300

    feat: vueuse

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, usePreferredDark } from "@vueuse/core";
 
-<template>
-  <h1>Hello World</h1>
-</template>
+const isPrefferedDark = usePreferredDark();
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+useDark({
+  attribute: "theme",
+  initialValue: isPrefferedDark ? "dark" : "light",
+});
+</script>
+
+<template></template>
+
+<style scoped></style>
